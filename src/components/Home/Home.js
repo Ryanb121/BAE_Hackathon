380a78c14ffd4d1a4366b79aa74aec9b4567653d
leigheastell <leigheastell1286@gmail.com>
2022-08-17 14:58:48 +0100

    homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,13 @@
+import "./Home.css"
+import image from "../../images/image.png"
+
 const Home = () => {
-    return ( <h1>Home</h1> );
+    return ( 
+    <div className="homecontainer">
+    <img className="image" src={image} alt="view"/>
+    <p>Welcome to the SCORE app. The idea behind the app is to help people understand the importance of reducing their carbon footprint and ways of being able to do so. Also some helpful tips and advise on how best to recycle materials. </p>
+    </div>
+    );
 }
  
 export default Home;
